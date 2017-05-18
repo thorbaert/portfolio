df38b44b74059a6c0a7ed13ff0783b296b27f0e4
Joshua Baert <jbghoring@gmail.com>
2017-05-17 20:33:51 -0500

    Getting Notified when Portfolio is viewed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const mailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const cors = require('cors');
 
 const config = require('./config');
@@ -15,10 +16,32 @@ let transporter = mailer.createTransport(config.smtpStr);
 const app = express();
 
 
+app.use(function (req,res,next) {
+	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	ip = ip.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
+//	console.log('Visitor from ip: ' + ip);
+	axios.get(`http://ip-api.com/json/${ip}`)
+		.then(function (ipTestResponse) {
+			console.log(ipTestResponse.data);
+			let email = {
+				from: 'visitors@baert.io',
+				to: 'developer@baert.io',
+				subject: 'Visitor at Portfolio',
+				text: `you have a portfolio Visitor at ip: ${ip} with the info of
+${JSON.stringify(ipTestResponse.data,null,'  ')}`
+			}
+			transporter.sendMail(email, (err, results) => {
+				if (err) {
+					console.log(err);
+				}
+			})
+		})
+	next()
+});
+
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(cors());
-
 
 app.post('/mail', function (req, res, next) {
 	console.log(req.body);
@@ -26,7 +49,7 @@ app.post('/mail', function (req, res, next) {
 	let email = {
 		from: 'portfolio@baert.io',
 		to: 'developer@baert.io',
-		subject: 'contact from your portfolio',
+		subject: 'Contact request from Portfolio',
 		text: `You've receive a contact request from ${body.name} 
 email : ${body.email}
 phone : ${body.phone}
