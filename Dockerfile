97afcbfa8c0d745b1d596407da6dd765e7e49ec2
JoshuaBaert <developer@baert.io>
2018-09-04 21:36:50 -0500

    Added Docker support

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+### Stage 1
+
+FROM node:8 as node
+LABEL author="Joshua Baert"
+RUN npm install -g gulp@3.9.1
+WORKDIR /build
+COPY ./public/package.json package.json
+RUN npm install
+COPY ./public .
+RUN gulp
+
+### Stage 2
+
+FROM node:8
+WORKDIR /app
+COPY ./package.json package.json
+RUN npm install
+COPY ./index.js index.js
+COPY ./config.js config.js
+COPY --from=node /build /app/public
+EXPOSE 8080
+ENTRYPOINT ["npm", "run", "start"]
+
--- index.js
+++ index.js
@@ -19,63 +19,61 @@ const app = express();
 
 
 
-
-
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/visit', function (req,res,next) {
-	let ipFull = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-	let ip = ipFull.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/);
-	if (knownIps.indexOf(ipFull) < 0) {
-		knownIps.push(ipFull);
-		axios.get(`http://ip-api.com/json/${ip}`)
-			.then(function (ipTestResponse) {
-				let email = {
-					from: 'visitors@baert.io',
-					to: 'developer@baert.io',
-					subject: `Visitor from ${ipTestResponse.data.city} ip: ${ip}`,
-					text: `you have a portfolio Visitor at ip: ${ip} with the info of
-${JSON.stringify(ipTestResponse.data,null,'  ')}`
-				};
-				transporter.sendMail(email, (err, results) => {
-					if (err) {
-						console.log(err);
-					} else {
-						res.sendStatus(200)
-					}
-					
-				})
-			})
-	}
-	
+app.get('/visit', function (req, res, next) {
+    let ipFull = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    let ip = ipFull.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/);
+    if (knownIps.indexOf(ipFull) < 0) {
+        knownIps.push(ipFull);
+        axios.get(`http://ip-api.com/json/${ip}`)
+            .then(function (ipTestResponse) {
+                let email = {
+                    from: 'visitors@baert.io',
+                    to: 'developer@baert.io',
+                    subject: `Visitor from ${ipTestResponse.data.city} ip: ${ip}`,
+                    text: `you have a portfolio Visitor at ip: ${ip} with the info of
+${JSON.stringify(ipTestResponse.data, null, '  ')}`,
+                };
+                transporter.sendMail(email, (err, results) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.sendStatus(200);
+                    }
+
+                });
+            });
+    }
+
 });
 
 app.post('/mail', function (req, res, next) {
-	let body = req.body;
-	let email = {
-		from: 'portfolio@baert.io',
-		to: 'developer@baert.io',
-		subject: 'Contact request from Portfolio',
-		text: `You've receive a contact request from ${body.name} 
+    let body = req.body;
+    let email = {
+        from: 'portfolio@baert.io',
+        to: 'developer@baert.io',
+        subject: 'Contact request from Portfolio',
+        text: `You've receive a contact request from ${body.name} 
 email : ${body.email}
 phone : ${body.phone}
 Message : ${body.message}
 `,
-	};
-	transporter.sendMail(email, (err, results) => {
-		if (err) {
-			console.log(err);
-			res.sendStatus(400)
-		}	else {
-			console.log(results);
-			res.sendStatus(200)
-		}
-	})
+    };
+    transporter.sendMail(email, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(400);
+        } else {
+            console.log(results);
+            res.sendStatus(200);
+        }
+    });
 });
 
 
 app.listen(config.port, () => {
-	console.log('listening on port ' + config.port)
+    console.log('listening on port ' + config.port);
 });
